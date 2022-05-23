7cd8d19c0dc41c1b5306ae837497097b74420a4d
ale_figure8 <gomeznieto@gmail.com>
2022-05-23 18:41:54 -0300

    better

--- 14.cpp
+++ 14.cpp
@@ -8,9 +8,12 @@ using namespace std;
 
 int main (void){
 
-  int vector_1[10];
-  int vector_2[5];
-  int vector_3[15];
+  const int v1 = 10;
+  const int v2 = 5;
+  const int v3 = v1 + v2;
+  int vector_1[v1];
+  int vector_2[v2];
+  int vector_3[v3];
   int cont = 0;
 
   for (int i = 0; i < 10; i++)
@@ -19,27 +22,31 @@ int main (void){
     cout << "Ingrese un numero para el vector numero 1: ";
     cin >> n;
     vector_1[i] = n;
-    vector_3[cont] = n;
-    cont++;
   }
-  
+
   for (int i = 0; i < 5; i++)
   {
     int n;
     cout << "Ingrese un numero para el vector numero 2: ";
     cin >> n;
     vector_2[i] = n;
-    vector_3[cont] = n;
-    cont++;
   }
 
   for (int i = 0; i < 15; i++)
   {
+    if(i < v1){
+      vector_3[i] = vector_1[i];
+
+    } else {
+      vector_3[i] = vector_2[cont];
+      cont++;
+    }
+  }
+
+  for (int i = 0; i < v3; i++)
+  {
     cout << vector_3[i] << endl;
   }
-  
-
-
 
   return 0;
 }
